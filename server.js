e3a9671107f03a0f15964118bf6517b4ac5aebb0
mjtedder <mjtedder@gmail.com>
2018-07-16 11:06:25 -0400

    finished initial server file: TODO: write routes, add models

--- server.js
+++ server.js
@@ -16,3 +16,32 @@ let PORT = 8080
 
 // Initialize express
 let app = express()
+
+// Configure middleware
+// Use morgan logger for logging requests
+app.use(logger('dev'))
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true}))
+// Use express.static to serve the public folder as a static directory
+app.use(express.static('public'))
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+// A GET route for scraping the news website
+
+// A route for getting all Articles from the db
+
+// A route for grabbing a specific Article by id, populate it with its comment.
+
+// A route for saving/updating an Article's associated Note
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("App is running on port " + PORT + "!")
+})
