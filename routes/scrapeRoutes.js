8d73235e1edcfa1fde32b2e3c9acd3581d2a4dc6
mjtedder <mjtedder@gmail.com>
2018-07-17 10:51:45 -0400

    Successfully scraped from Denver Post. TODO: write Routes, add shcema for Comment

--- routes/scrapeRoutes.js
+++ routes/scrapeRoutes.js
@@ -1,6 +1,31 @@
 // DEPENDENCIES===============================================
+// Tells app where to look for models
 let db = require('./models')
+
+// Scraping tools
 let axios = require('axios')
 let cheerio = require('cheerio')
 
-// A GET route for scraping the news website
+// A GET route for scraping the Reddit gaming news website
+app.get('/scrape', (req, res) => {
+  axios.get('https://www.denverpost.com/').then((response) => {
+    let $ = cheerio.load(response.data)
+
+    $('h4.entry-title').each((i, element) => {
+      let result = {}
+
+      result.headline = $(this).children('a.article-title').attr('title')
+      result.summary = $('div.article-info').children('div.excerpt').text()
+      result.url = $(this).children().attr('href')
+
+      db.Article.create(result)
+        .then((dbArticle) => {
+          console.log(dbArticle)
+        })
+        .catch((err) => {
+          return res.json(err)
+        })
+    })
+    res.send("Scrape Complete")
+  })
+})
